b1eb955ef6923614f9452d10303fee421643eda6
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-05-31 13:50:55 +0900

    first

---
+++ src/asset/Icons.tsx
@@ -0,0 +1,13 @@
+export const Icons = {
+    home: "/images/Home.svg",
+    heart: "/images/Heart.svg",
+    filledHeart: "/images/Filled Heart.svg",
+    picture: "/images/Picture.svg",
+    Profile: "/images/Profile.svg",
+    plus: "/images/Plus.svg",
+    x: "/images/X.svg"
+}
+
+export const Images = {
+    logo: "/images/logo.png",
+}
---
+++ src/component/header/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <header>
+            <div className={"top-header"}>
+                <img src="/logo.png" alt="logo" height={"70px"}/>
+            </div>
+            <div className={"bottom-header"}>
+            </div>
+        </header>
+    )
+}
