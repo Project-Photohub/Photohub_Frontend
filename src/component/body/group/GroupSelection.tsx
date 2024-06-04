a180168fb61afab8af3e34ce73f5c5f940340ee2
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-04 10:29:45 +0900

    FEAT :: GroupSelection 선택 기능 구현

--- src/component/body/group/GroupSelection.tsx
+++ src/component/body/group/GroupSelection.tsx
@@ -6,8 +6,18 @@ export interface GroupSelectionProps {
 }
 
 export const GroupSelection = (props: GroupSelectionProps) => {
-    return <div key={`group-${props.name}`}
-        className={`${props.isSelected ? "bg-background-highlight" : "bg-background-hover hover:bg-background-highlight"} flex items-center gap-[10px] h-[70px] rounded-[30px] pe-[20px] pl-[20px] pt-[10px] pb-[10px]`}>
+    return <div
+        key={`group-${props.name}`}
+        className={`${props.isSelected ? "bg-background-highlight" : "bg-background-hover hover:bg-background-highlight"} flex items-center gap-[10px] h-[70px] rounded-[30px] pe-[20px] pl-[20px] pt-[10px] pb-[10px] animated`}
+        onClick={() => {
+            // eslint-disable-next-line no-restricted-globals
+            const paths = location.href.split('/')
+
+            const currentGroupAndMemberIdSize = paths[paths.length - 1].length + paths[paths.length - 2].length + 1
+
+            // eslint-disable-next-line no-restricted-globals
+            location.href = location.href.substring(0, location.href.length - currentGroupAndMemberIdSize) + props.id.toString() + "/1"
+        }}>
         <img className={"h-[50px] w-[50px]"} src={props.logo} alt={props.name} height={"50px"} width={"50px"}/>
         <p className={"font-p-regular text-text-white text-[32px]"}>{props.name}</p>
     </div>
