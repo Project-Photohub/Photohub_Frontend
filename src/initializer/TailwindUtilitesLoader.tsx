03cdc981c063c52954b37a13d57e25b4eb4ff407
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-05 13:04:09 +0900

    FEAT :: Tailwind Css Loader

---
+++ src/initializer/TailwindUtilitesLoader.tsx
@@ -0,0 +1,3 @@
+export const TailwindUtilitesLoader = () => {
+    return <div className={`invisible hidden w-[220px] w-[330px] w-[340px] w-[510px] h-[220px] h-[330px] h-[340px] h-[510px]`}></div>
+}
