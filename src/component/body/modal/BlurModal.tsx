74aa535508d68a4d004470e8fe2098a07218007a
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-06 11:34:00 +0900

    FEAT :: 모달 컴포넌트화

--- src/component/body/modal/BlurModal.tsx
+++ src/component/body/modal/BlurModal.tsx
@@ -1,43 +1,34 @@
-import {useState} from "react";
-
 export interface BlurModalProps {
-    child: JSX.Element
+    children: JSX.Element[],
+    setIsModalOpened: (value: (((prevState: boolean) => boolean) | boolean)) => void
 }
 
-export const BlurModal = (props: BlurModalProps) => {
-
-    const [
-        isModalOpened, // Use to open/close modal.
-        setIsModalOpened // Set it.
-    ] = useState<boolean>(false)
+export const BlurModal = (
+    props: BlurModalProps
+) => {
 
     return (
-        // Modal Wrapper, Generator. For implement Modal Open/Close
-        <div className={"invisible"}>
+        /* Blur, Has modal div on child. */
+        <div
+            className={"fixed top-0 bottom-0 left-0 w-screen h-screen bg-blur-default overflow-hidden"}
+            onClick={() => {
+                props.setIsModalOpened(false)
+            }}>
 
-            {/* Blur, Has modal div on child. Closeable */}
-            {isModalOpened &&
+            {/* Modal Wrapper, Align Modal */}
+            <div className={"flex w-full h-full justify-center items-center"}>
+
+                {/* The Modal. */}
                 <div
-                    className={"fixed top-0 bottom-0 left-0 w-screen h-screen bg-blur-default overflow-hidden"}
-                    onClick={() => {
-                        setIsModalOpened(false)
+                    className={"flex flex-col gap-[30px] items-center rounded-[30px] bg-background-highlight w-[1000px] pt-[20px] pb-[20px] pl-[50px] pe-[50px]"}
+                    onClick={(event) => {
+                        event.stopPropagation()
                     }}>
 
-                    {/* Modal Wrapper, Align Modal */}
-                    <div className={"flex w-full h-full justify-center items-center"}>
-
-                        {/* The Modal. */}
-                        <div
-                            className={"flex flex-col gap-[30px] items-center rounded-[30px] bg-background-highlight w-[1000px] pt-[20px] pb-[20px] pl-[50px] pe-[50px]"}
-                            onClick={(event) => {
-                                event.stopPropagation()
-                            }}>
-
-                            {/* Modal's content. */}
-                            {props.child}
-                        </div>
-                    </div>
-                </div>}
+                    {/* Modal's content. */}
+                    {props.children}
+                </div>
+            </div>
         </div>
     )
 }
--- src/component/body/photocard/PhotoCardModal.tsx
+++ src/component/body/photocard/PhotoCardModal.tsx
@@ -1,27 +1,16 @@
 import {PhotoCardImage} from "./PhotoCardImage";
 import {PhotoCardProps} from "./PhotoCard";
+import {BlurModal} from "../modal/BlurModal";
 
 export const PhotoCardModal = (
     props: PhotoCardProps,
     setIsModalOpened: (value: (((prevState: boolean) => boolean) | boolean)) => void
 ) => {
     return (
-        <div id={"photo-card-modal"}
-             className={"fixed top-0 bottom-0 left-0 w-screen h-screen bg-blur-default overflow-hidden"}
-             onClick={(event) => {
-                 setIsModalOpened(false)
-             }}>
-            <div className={"flex w-full h-full justify-center items-center"}>
-                <div
-                    className={"flex flex-col gap-[30px] items-center rounded-[30px] bg-background-highlight w-[1000px] pt-[20px] pb-[20px] pl-[50px] pe-[50px]"}
-                    onClick={(event) => {
-                        event.stopPropagation()
-                    }}>
-                    {PhotoCardModalInfo(props)}
-                    {PhotoCardModalImages(props)}
-                </div>
-            </div>
-        </div>
+        <BlurModal setIsModalOpened={setIsModalOpened}>
+            {PhotoCardModalInfo(props)}
+            {PhotoCardModalImages(props)}
+        </BlurModal>
     )
 }
 
