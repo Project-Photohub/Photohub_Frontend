2bd4bab9d5a0c6854283eff5488fd6ec03b84ef5
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-10 09:26:31 +0900

    BURN :: Deleted Feature.

--- src/component/header/HeaderButtons.tsx
+++ src/component/header/HeaderButtons.tsx
@@ -2,24 +2,24 @@ import {HeaderButton} from "./HeaderButton";
 
 export const HeaderButtons = () => {
     return (<div className={"gap-2.5 flex"}>
-            <HeaderButton
+            {/*<HeaderButton
                 iconUri={"/Picture.svg"}
                 onClick={() => {
                 }}
                 text={"내 포토카드"}
-            />
+            />*/}
             <HeaderButton
                 iconUri={"/Plus.svg"}
                 onClick={() => {
                 }}
                 text={"포토카드 추가"}
             />
-            <HeaderButton
+            {/*<HeaderButton
                 iconUri={"/Profile.svg"}
                 onClick={() => {
                 }}
                 text={"로그인"}
-            />
+            />*/}
         </div>
     )
 }
