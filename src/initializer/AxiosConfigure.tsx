c633ea7b7d527d3506fac533a45b33ebb889340d
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-25 15:25:12 +0900

    FIX :: CORS

--- src/initializer/AxiosConfigure.tsx
+++ src/initializer/AxiosConfigure.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import {BASE_URL, FRONT_BASE_URL} from "../module/request/ServerInfo";
+import {BASE_URL, FRONT_BASE_URL, HttpMethod} from "../module/request/ServerInfo";
 
 export const AxiosConfigure = () => {
     axios.defaults.baseURL = BASE_URL
     axios.defaults.headers.common["Content-Type"] = "application/json"
     axios.defaults.headers.common["Content-Encoding"] = "UTF-8"
     axios.defaults.headers.common["Access-Control-Request-Origin"] = FRONT_BASE_URL
+    axios.defaults.headers.common["Access-Control-Request-Method"] = [HttpMethod.GET]
+    axios.defaults.headers.common["Access-Control-Request-Headers"] = ["Authorization", "Content-Type", "Accept"]
 
     return <></>
 }
