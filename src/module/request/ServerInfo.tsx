f617d8ee216258b73300e1932d9dfb9acab81852
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-26 10:21:19 +0900

    배포포

--- src/module/request/ServerInfo.tsx
+++ src/module/request/ServerInfo.tsx
@@ -12,8 +12,8 @@ let BASE_URL_STORAGE: string = ""
 let FRONT_BASE_URL_STORAGE: string = ""
 
 if (import.meta.env.VITE_ENVIRONMENT == "prod") {
-    BASE_URL_STORAGE = import.meta.env.VITE_BASE_URL
-    FRONT_BASE_URL_STORAGE = import.meta.env.VITE_FRONT_BASE_URL
+    BASE_URL_STORAGE = import.meta.env.VITE_PROD_BASE_URL
+    FRONT_BASE_URL_STORAGE = import.meta.env.VITE_FRONT_PROD_BASE_URL
 } else if (import.meta.env.VITE_ENVIRONMENT == "stag") {
     BASE_URL_STORAGE = import.meta.env.VITE_STAG_BASE_URL
     FRONT_BASE_URL_STORAGE = import.meta.env.VITE_FRONT_STAG_BASE_URL
