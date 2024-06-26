80945712e15c143d650ce899ea5b066eb24263ae
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-26 11:13:33 +0900

    CONFIG :: CORS

--- src/initializer/AxiosConfigure.tsx
+++ src/initializer/AxiosConfigure.tsx
@@ -5,7 +5,6 @@ export const AxiosConfigure = () => {
     axios.defaults.baseURL = BASE_URL
     axios.defaults.headers.common["Content-Type"] = "application/json"
 
-    axios.defaults.headers.common["Access-Control-Request-Origin"] = FRONT_BASE_URL
     axios.defaults.withCredentials = true
 
     return <></>
