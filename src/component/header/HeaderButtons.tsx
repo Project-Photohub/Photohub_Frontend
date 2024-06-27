3ca7ef1039d28e421943908039cde61923bdcbd0
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-27 23:43:03 +0900

    FEAT :: Login 버튼 추가

--- src/component/header/HeaderButtons.tsx
+++ src/component/header/HeaderButtons.tsx
@@ -1,6 +1,39 @@
 import {HeaderButton} from "./HeaderButton";
+import {useState} from "react";
+import axios, {AxiosError} from "axios";
+import {HttpMethod} from "../../module/request/ServerInfo";
 
 export const HeaderButtons = () => {
+
+    const [authButton, setAuthButton]
+        = useState<JSX.Element>(
+        <HeaderButton
+            iconUri={"/Profile.svg"}
+            text={"로그인"}
+            gotoPath={"/auth/login"}
+        />)
+
+    if (document.cookie.includes("SESSIONID")) {
+
+        axios.request({
+            method: HttpMethod.GET,
+            url: "/user"
+        }).then((response) => {
+            setAuthButton(
+                HeaderButton({
+                    iconUri: "/Profile.svg",
+                    text: response.data.nickname,
+                    gotoPath: ""
+                })
+            )
+        }).catch((err: AxiosError) => {
+            if (err.response?.status === 401) {
+                document.cookie = "SESSIONID=; expire=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+            }
+            console.log(err)
+        })
+    }
+
     return (<div className={"gap-2.5 flex"}>
             {/*<HeaderButton
                 iconUri={"/Picture.svg"}
@@ -13,11 +46,7 @@ export const HeaderButtons = () => {
                 text={"포토카드 추가"}
                 gotoPath={"/add"}
             />
-            {/*<HeaderButton
-                iconUri={"/Profile.svg"}
-                text={"문의하기"}
-                gotoPath={"/ask"}
-            />*/}
+            {authButton}
         </div>
     )
 }
