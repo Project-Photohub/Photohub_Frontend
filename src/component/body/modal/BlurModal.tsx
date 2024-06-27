a38c20ae32388d9e2c3c56ff82574416769569c5
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-27 16:03:16 +0900

    FEAT :: BlurModal's onClose Function

--- src/component/body/modal/BlurModal.tsx
+++ src/component/body/modal/BlurModal.tsx
@@ -1,6 +1,7 @@
 export interface BlurModalProps {
     children: JSX.Element[],
-    setModel: (value: JSX.Element | undefined) => void
+    setModel: (value: JSX.Element | undefined) => void,
+    onClose?: () => void
 }
 
 export const BlurModal = (
@@ -13,6 +14,7 @@ export const BlurModal = (
             className={"fixed top-0 bottom-0 left-0 w-screen h-screen bg-blur-default overflow-hidden"}
             onClick={() => {
                 props.setModel(undefined)
+                props.onClose && props.onClose()
             }}>
 
             {/* Modal Wrapper, Align Modal */}
@@ -20,7 +22,7 @@ export const BlurModal = (
 
                 {/* The Modal. */}
                 <div
-                    className={"flex flex-col gap-[30px] items-center rounded-[30px] bg-background-highlight w-[1000px] pt-[20px] pb-[20px] pl-[50px] pe-[50px] shadow-blur-modal"}
+                    className={"flex flex-col gap-[30px] items-center rounded-[30px] bg-background-highlight pt-[20px] pb-[20px] pl-[50px] pe-[50px] shadow-blur-modal"}
                     onClick={(event) => {
                         event.stopPropagation()
                     }}>
