5b2d4e37d96cd46463a177663c58527ced322e60
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-29 23:16:50 +0900

    REFACT :: Side Toast Size Change

--- src/component/body/toast/CompleteCopyToast.tsx
+++ src/component/body/toast/CompleteCopyToast.tsx
@@ -9,7 +9,7 @@ export const CompleteCopyToast = (props: CompleteCopyToastProps) => {
     return <Toast
         currentToast={props.toast}
         setToast={props.setToast}>
-        <img src={"/Check.svg"} width={"40px"} alt={""}/>
-        <p className={"text-text-white text-[30px] font-p-regular"}>링크가 복사되었습니다!</p>
+        <img src={"/Check.svg"} width={"30px"} alt={""}/>
+        <p className={"text-text-white text-[24px] font-p-regular"}>링크가 복사되었습니다!</p>
     </Toast>
 }
