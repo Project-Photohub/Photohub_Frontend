2de63700e3d6c420b029922d09562175564defec
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-07-01 21:43:10 +0900

    FIX :: ...

--- src/component/body/photocard/add/InputPhotoCardTitle.tsx
+++ src/component/body/photocard/add/InputPhotoCardTitle.tsx
@@ -8,15 +8,15 @@ export const InputPhotoCardTitle = (props: PhotoCardNameInputProps) => {
             <div
                 className={"flex border-background-highlight border-solid border-[3px] rounded-[30px] pl-[80px] pr-[80px] pt-[20px] pb-[20px]"}>
                 <input type={"text"} placeholder={"멋진 이름을 지어주세요!"}
-                       className={"bg-background-default placeholder:text-text-subtext3 placeholder:font-p-light outline-0  font-p-light text-text-white text-[36px]"}
-                       onInput={(input) => {
-                           const text = (input.target as HTMLInputElement).value
+                       className={"bg-background-default placeholder:text-text-subtext3 placeholder:font-p-light outline-0 min-w-[650px] font-p-light text-text-white text-[36px]"}
                        maxLength={30}
+                       onInput={(event) => {
+                           const text = (event.target as HTMLInputElement).value
 
                            if (text === null || text === undefined || text.trim() === "") {
-                               props.setTitle(null)
+                               props.setTitle(null);
                            } else {
-                               props.setTitle((input.target as HTMLInputElement).value)
+                               props.setTitle((event.target as HTMLInputElement).value);
                            }
                        }}
                 />
